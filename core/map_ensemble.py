7a60845cb281243051d4e338c9d6bc0d0fed800f
phumm <philipp.hummel1993@gmail.com>
2020-03-31 11:55:53 +0200

    map ensemble made up of individual map networks as separate class

--- core/map_ensemble.py
+++ core/map_ensemble.py
@@ -11,6 +11,7 @@ class MapEnsemble:
         input_shape=[1],
         layer_units=[200, 100, 1],
         layer_activations=["relu", "relu", "linear"],
+        learning_rate=0.01,  # can be float or an instance of tf.keras.optimizers.schedules
         seed=0,
     ):
         """Class that represents an ensemble of Map (currently ML) trained neural networks.
@@ -23,27 +24,24 @@ class MapEnsemble:
         self.input_shape = input_shape
         self.layer_units = layer_units
         self.layer_activations = layer_activations
+        self.learning_rate = learning_rate
         self.seed = seed
         tf.random.set_seed(self.seed)
         self.networks = [
-            build_model(self.input_shape, self.layer_units, self.layer_activations)
+            MapNetwork(
+                self.input_shape,
+                self.layer_units,
+                self.layer_activations,
+                self.learning_rate,
+                seed=self.seed + i,
+            )
             for i in range(self.n_networks)
         ]
 
-        initial_learning_rate = 0.01
-        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-            initial_learning_rate, decay_steps=20, decay_rate=0.9, staircase=True
-        )
-        for network in self.networks:
-            network.compile(
-                optimizer=tf.keras.optimizers.Adam(lr_schedule),
-                loss="mean_squared_error",
-            )
-
-    def train(self, x_train, y_train, batchsize_train, epochs=120):
+    def train(self, x_train, y_train, batch_size, epochs=120):
         tf.random.set_seed(self.seed)
         for network in self.networks:
-            network.fit(x_train, y_train, epochs=epochs, batch_size=batchsize_train)
+            network.train(x_train, y_train, batch_size=batch_size, epochs=epochs)
 
     def predict(self, x_test):
         predictions = []
@@ -51,3 +49,39 @@ class MapEnsemble:
             prediction = network(x_test)
             predictions.append(prediction)
         return predictions
+
+
+class MapNetwork:
+    def __init__(
+        self,
+        input_shape=[1],
+        layer_units=[200, 100, 1],
+        layer_activations=["relu", "relu", "linear"],
+        learning_rate=0.01,  # can be float or an instance of tf.keras.optimizers.schedules
+        seed=0,
+    ):
+        self.input_shape = input_shape
+        self.layer_units = layer_units
+        self.layer_activations = layer_activations
+        self.learning_rate = learning_rate
+        self.seed = seed
+        tf.random.set_seed(self.seed)
+        self.network = build_model(
+            self.input_shape, self.layer_units, self.layer_activations
+        )
+
+        self.network.compile(
+            optimizer=tf.keras.optimizers.Adam(self.learning_rate),
+            loss="mean_squared_error",
+        )
+
+    def train(self, x_train, y_train, batch_size, epochs=120):
+        tf.random.set_seed(self.seed)
+        self.network.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
+
+    def predict(self, x_test):
+        prediction = self.network(x_test)
+        return prediction
+
+    def __call__(self, x_test):
+        return self.predict(x_test)
